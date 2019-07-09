d8e1f51223cef299d12ac14fb949f2b58f831bed
fahlmant <fahlmantaylor@gmail.com>
2019-07-08 21:17:13 -0400

    Reorganize pkg into print functions and generate functions
    Split up the sudokugen pkg into two files; sudokugen.go holds fucntions with logic for generating the board, as well as the structs while boardprint.go hold helper functions for various printing options.

--- pkg/sudokugen/sudokugen.go
+++ pkg/sudokugen/sudokugen.go
@@ -1,7 +1,6 @@
 package sudokugen
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
@@ -119,49 +118,4 @@ func FillNumber(x int, y int) {
 			grid[x][y] = num
 		}
 	}
-}
-
-func printRowOfSquares(topLeft Coord, bottomRight Coord) {
-
-	for y := topLeft.y; y <= bottomRight.y; y++ {
-		for x := topLeft.x; x <= bottomRight.x; x++ {
-			fmt.Print(grid[y][x])
-			fmt.Print(" ")
-			if (x+1)%4 == 0 {
-				fmt.Print(" ")
-				fmt.Print(" ")
-				fmt.Print(" ")
-			}
-		}
-		fmt.Println("")
-	}
-	fmt.Println("")
-}
-
-func printColumnOfSquares(topLeft Coord, bottomRight Coord) {
-
-}
-
-func printSquare(top Coord, bottom Coord) {
-
-	for y := top.y; y <= bottom.y; y++ {
-		for x := top.x; x <= bottom.x; x++ {
-			fmt.Print(grid[y][x])
-			fmt.Print(" ")
-		}
-		fmt.Println("")
-	}
-	fmt.Println("")
-}
-
-func PrintBoard() {
-
-	for x := 0; x < 12; x++ {
-		fmt.Println(grid[x])
-	}
-	fmt.Println("")
-	printRowOfSquares(square1Top, square3Bottom)
-	printRowOfSquares(square4Top, square6Bottom)
-	printRowOfSquares(square7Top, square9Bottom)
-	printRowOfSquares(square10Top, square12Bottom)
 }
